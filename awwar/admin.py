a8c6897616b290ea4565105941e198a98ae780e2
seron-ux <abyseronkirui@gmail.com>
2021-04-11 20:43:39 +0300

    form a class method for search

--- awwar/admin.py
+++ awwar/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-# from .models import Post
-# admin.site.site_header='seron'
+from .models import Post
+admin.site.site_header='seron'
 
-# # Register your models here.
-# admin.site.register(Post)
+
+admin.site.register(Post)
